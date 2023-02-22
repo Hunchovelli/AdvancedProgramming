e3d0363b0ec7e46dcefd8274c11ea1b9a60293a5
Myron-Dias <121821776+Myron-Dias@users.noreply.github.com>
2023-02-22 16:07:23 +0000

    Update GetServerInfo.java

--- GetServerInfo.java
+++ GetServerInfo.java
@@ -1,5 +1,5 @@
-// This class makes the GUI where the users enter the IP, PORT, etc...
-// This class is called for every new user
+//This class makes the GUI where the users enter the IP, PORT, etc...
+//This class is called for every new user
 
 import java.awt.GridLayout;
 
@@ -20,33 +20,36 @@ public class GetServerInfo {
 	public String[] getServerInfo(JFrame frame)
 	{
 		  JPanel myPanel = new JPanel(new GridLayout(4, 1));
-    	myPanel.add(new JLabel("Enter the server ip:"));
-    	myPanel.add(ip);
-    	myPanel.add(new JLabel("Enter the server port:"));
-    	myPanel.add(port);
-    	myPanel.add(new JLabel("Enter your ip address:"));
-    	myPanel.add(my_ip);
-    	myPanel.add(new JLabel("Enter your port(optional):"));
-    	myPanel.add(my_port);
-    	
-    	
-    	int result = JOptionPane.showConfirmDialog(frame, myPanel, 
+ 	myPanel.add(new JLabel("Enter the server ip:"));
+ 	myPanel.add(ip);
+ 	myPanel.add(new JLabel("Enter the server port:"));
+ 	myPanel.add(port);
+ 	myPanel.add(new JLabel("Enter your ip address:"));
+ 	myPanel.add(my_ip);
+ 	myPanel.add(new JLabel("Enter your port(optional):"));
+ 	myPanel.add(my_port);
+ 	
+ 	
+ 	int result = JOptionPane.showConfirmDialog(frame, myPanel, 
 	               "Please Enter Details", JOptionPane.OK_CANCEL_OPTION);
-    	
-    	if (result == JOptionPane.OK_OPTION) {
+ 	
+ 	if (result == JOptionPane.OK_OPTION) {
 	         vals[0] = ip.getText();
 	         vals[1] = port.getText();
 	         vals[2] = my_ip.getText();
 	      }
-    	
-    	String port_option = my_port.getText();
-    	
-    	if (port_option != null)
-    	{
-    		vals[3] = port_option;
-    	}
-    	   	
-    	return vals;
+ 	
+ 	String port_option = my_port.getText();
+ 	
+ 	// Check if the client has provided an optional port or not
+ 	if (port_option.equals(""))
+ 	{
+ 		vals[3] = "None";
+ 	}
+ 	
+ 	else vals[3] = port_option;
+ 	   	
+ 	return vals;
 	}
 
 }
